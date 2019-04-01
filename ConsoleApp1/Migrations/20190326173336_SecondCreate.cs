f4149ab34021abaa64fa6e16816598a93eae96ce
Julio Ramirez <julio@teamramirez.com>
2019-04-01 11:52:20 -0400

    Second Commit

---
+++ ConsoleApp1/Migrations/20190326173336_SecondCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsoleApp1.Migrations
+{
+    public partial class SecondCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PostId",
+                table: "Posts",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "BlogId",
+                table: "Blogs",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Posts",
+                newName: "PostId");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Blogs",
+                newName: "BlogId");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,93 @@
+﻿using ConsoleApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in one of the following commands:");
+            Console.WriteLine("AddBlogs - This will add urls to the Blog table");
+            Console.WriteLine("ShowBlogs - This will display all the blogs in the Blogs table");
+            Console.WriteLine("DeleteBlogs - This will delete all the blogs in the Blogs table");
+            prompt();
+        }
+
+        static void prompt()
+        {
+            Console.Write("Please enter a command: ");
+            string command = Console.ReadLine();
+
+            if (command == "AddBlogs")
+                addBlogValues();
+            else if (command == "ShowBlogs")
+                showBlogValues();
+            else if (command == "DeleteBlogs")
+                deleteBlogValues();
+
+        }
+
+
+        static void addBlogValues()
+        {
+            using (var db = new ModelContext())
+            {
+                db.Blogs.Add(new Blog { Url = "https://google.com" });
+                db.Blogs.Add(new Blog { Url = "https://yahoo.com" });
+                db.Blogs.Add(new Blog { Url = "https://bing.com" });
+                db.Blogs.Add(new Blog { Url = "https://myspace.com" });
+                db.Blogs.Add(new Blog { Url = "https://shazam.com" });
+
+                var count = db.SaveChanges();
+
+                db.Dispose();
+
+                Console.WriteLine("{0} records have been added to the database.", count.ToString());
+            }
+
+            prompt();
+        }
+
+        static void deleteBlogValues()
+        {
+            using (var db = new ModelContext())
+            {
+                var allrecords = from x in db.Blogs select x;
+                db.Blogs.RemoveRange(allrecords);
+
+                var count = db.SaveChanges();
+
+                db.Dispose();
+
+                Console.WriteLine("{0} records have been removed to the database.", count.ToString());
+            }
+
+            prompt();
+        }
+
+        static void showBlogValues()
+        {
+            using (var db = new ModelContext())
+            {
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+
+                var count = db.Blogs.Count();
+                db.Dispose();
+
+                Console.WriteLine("{0} records have been displayed.", count.ToString());
+            }
+
+            prompt();
+        }
+    }
+}
